92fcade700af7ed2c057ecfa4e3dac11d961de96
Shakib-BD <its.shakib@proton.me>
2024-12-02 07:46:30 +0000

    Sweet2: Define hardware/xiaomi in vendorsetup.sh
    Signed-off-by: Shakib-BD <its.shakib@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ cd kernel/xiaomi/sm6150
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 touch Android.mk
 cd ../../../
+git clone -b 14.0 https://github.com/crdroidandroid/android_hardware_xiaomi.git hardware/xiaomi
